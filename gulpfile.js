cd57bf7d387f233106f2f28fa2e4cb6db6d80672
Gerson Uriarte <gersongru@huskers.unl.edu>
2020-11-27 17:22:56 -0600

    Added to .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var gulp	= require('gulp');
-var deploy	= require('gulp-gh-pages');
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
 
-gulp.task('deploy',function (){
-	return gulp.src("./dis/**/*")
-	.pipe(deploy())
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
 });
